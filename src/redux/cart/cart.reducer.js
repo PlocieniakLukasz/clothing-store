908bfc0714cf076c4fc063753d5824234e871594
unknown <plocieniak@interia.pl>
2020-08-06 10:49:22 +0200

    added checkout component and user selectors

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
-import CartActionTypes from './cart.types';
+import CartActionTypes from "./cart.types";
+import { addItemToCart } from "./cart.utils";
+import { addItem } from "./cart.actions";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,8 +12,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
-      }
+        hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
